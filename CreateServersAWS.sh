2468d3d6574099887605b09545fb0e64f11e0021
Tejag142 <tejagrandhi142@gmail.com>
2025-09-02 20:58:39 +0530

    Create Instances-7

--- CreateServersAWS.sh
+++ CreateServersAWS.sh
@@ -6,9 +6,12 @@ Security_Group_ID="sg-01c0372b028981e8c"
 Zone_ID="Z0820810MDVVL6POTF7"
 DNS="ajet142.store"
 #Instances=(mongodb redis "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-Instances=(mongodb redis)
+Instances=("mongodb" "frontend")
+array=("A" "B" "ElementC" "ElementE")
 
-for instances in $(Instances[@])
+echo "$(array)"
+
+for instances in "$(Instances[@])"
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [$instances!="frontend"]
